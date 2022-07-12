763b799b8172726b5688459dd6f68cd915a5e47e
Habib <habib23me@gmail.com>
2022-07-12 16:26:12 +0300

    1.1.2

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,18 +1,18 @@
 import { JsonMessageBroker } from '../json_message_broker';
 test('Send with reply works', async () => {
- try{
-  const broker=await JsonMessageBroker.getInstance();
-  const dataToSend = { data: 'hello' };
-  const dataToReply = { data: 'hey' };
+  try {
+    const broker = await JsonMessageBroker.getInstance();
+    const dataToSend = { data: 'hello' };
+    const dataToReply = { data: 'hey' };
 
-  const queue = 'test';
-  broker.subscribe(queue, () => dataToReply);
-  const data = await broker.sendWithReply(queue, dataToSend);
-  console.log("Got data");
+    const queue = 'test';
+    broker.subscribe(queue, () => dataToReply);
+    const data = await broker.sendWithReply(queue, dataToSend);
+    console.log('Got data');
 
-  console.log(data);
-  expect(data).toEqual(dataToReply);
- }catch(e){
-  console.log(e);
- }
+    console.log(data);
+    expect(data).toEqual(dataToReply);
+  } catch (e) {
+    console.log(e);
+  }
 }, 1200000);
